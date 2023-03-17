111fdb1431a7dd09683eb8664a1ff6233f38e704
gippster <rsaybullin@gmail.com>
2023-03-18 00:03:44 +0600

    сделал функцию Parser и связал main с функцией, вывод в файл departments

---
+++ Parser.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def parse():
+    url = 'https://omgtu.ru/general_information/the-structure/the-department-of-university.php'
+    page = requests.get(url)
+    print(page.status_code)
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    block = soup.findAll('div', id='pagecontent')
+    description = ''
+    for data in block:
+        if data.find('li'):
+            description = data.text
+
+    return (description)
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from bs4 import BeautifulSoup
-import requests
-def parse():
-    url = 'https://omgtu.ru/general_information/the-structure/the-department-of-university.php' # передаем необходимы URL адрес
-    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
-    print(page.status_code) # смотрим ответ
-    soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
+import Parser
 
-    block = soup.findAll('div', class_='pagecontent') # находим  контейнер с нужным классом
-    description = ''
-    for data in block: # проходим циклом по содержимому контейнера
-        if data.find('a'): # находим тег <p>
-            description = data.text # записываем в переменную содержание тега
-
-    print(description)
-parse()
+if __name__ == '__main__':
+    f = open("departments.txt", "w")
+    departments = Parser.parse()
+    f.write(departments.replace('  ', ' ')) #Замена nonBreakSpace на обычные пробелы
